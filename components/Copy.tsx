e1ef66f63ed15acb911a195ba19e3e3e9ec09a77
Beau Hawkinson <hawkinson.beau@yahoo.com>
2022-08-15 18:34:02 -0500

    lighthouse improvements

--- components/Copy.tsx
+++ components/Copy.tsx
@@ -11,13 +11,20 @@ export default function CopyHelper({ toCopy }: CopyProps) {
 
   return (
     <button
+      aria-label="justify"
       className="flex-start bg-gray-200"
       onClick={() => setCopied(toCopy)}
     >
       {isCopied ? (
-        <ClipboardCheckIcon className="h-4 w-4  text-black md:h-6 md:w-6" />
+        <ClipboardCheckIcon
+          aria-hidden="true"
+          className="h-4 w-4  text-black md:h-6 md:w-6"
+        />
       ) : (
-        <ClipboardIcon className="inline-flex h-4 w-4   text-black md:h-6 md:w-6" />
+        <ClipboardIcon
+          aria-hidden="true"
+          className="inline-flex h-4 w-4   text-black md:h-6 md:w-6"
+        />
       )}
     </button>
   );
