6b9e878cf3d9889ce0f12df4521e06bfa1750fe6
Beau Hawkinson <hawkinson.beau@yahoo.com>
2022-09-27 14:27:07 -0500

    change landing page flex box

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import Link from "next/link"
 
 export default function Home() {
     return (
-        <div className="h-screen w-full space-y-12 bg-violet-200 p-12">
+        <div className="flex h-screen w-full flex-col items-center justify-center space-y-12 bg-violet-200 p-12">
             <Link href="/shapes">
-                <div className="mx-auto max-w-7xl rounded-3xl py-8 px-4 ring-[8px] ring-gray-200 hover:cursor-pointer hover:bg-gray-200 sm:py-24 sm:px-6 lg:flex lg:justify-between lg:px-8">
+                <div className="mx-auto w-full max-w-7xl  rounded-3xl py-8 px-4 ring-[8px] ring-gray-200 hover:cursor-pointer hover:bg-gray-200 sm:py-24 sm:px-6 lg:flex lg:justify-between lg:px-8">
                     <div className="mx-auto max-w-xl text-center">
                         <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl lg:tracking-tight">
                             View Shapes
@@ -16,14 +16,14 @@ export default function Home() {
                 </div>
             </Link>
             <Link href="/components">
-                <div className="mx-auto  max-w-7xl rounded-3xl py-8 px-4 ring-[8px] ring-gray-200 hover:cursor-pointer hover:bg-gray-200 sm:py-24 sm:px-6 lg:flex lg:justify-between lg:px-8">
+                <div className="mx-auto  w-full max-w-7xl rounded-3xl py-8 px-4 ring-[8px] ring-gray-200 hover:cursor-pointer hover:bg-gray-200 sm:py-24 sm:px-6 lg:flex lg:justify-between lg:px-8">
                     <div className="mx-auto max-w-xl text-center">
                         <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl lg:tracking-tight">
                             View Components
                         </h2>
                         <p className="mt-5 text-xl text-gray-500">
-                            View a collection of components including a coin-flip animation, curved
-                            text, and a responsive marquee.
+                            View a collection of c w-fullomponents including a coin-flip animation,
+                            curved text, and a responsive marquee.
                         </p>
                     </div>
                 </div>
